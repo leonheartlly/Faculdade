74fe651de7936c5e217a0279dafb8b3a295a5284
tatilattanzi <lattanzi.tati@gmail.com>
2016-04-14 20:29:03 -0300

    Revert "Testes JUnit: POST "
    This reverts commit 197c20057551a5f6fcb0500db788086b809e14e0.

--- Webproject/src/org/unitri/ppi2/rest/locacao/LocacaoTest.java
+++ Webproject/src/org/unitri/ppi2/rest/locacao/LocacaoTest.java
@@ -1,9 +1,6 @@
 package org.unitri.ppi2.rest.locacao;
 
 import static org.junit.Assert.*;
-
-import java.math.BigDecimal;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -15,12 +12,6 @@ import org.glassfish.jersey.client.ClientConfig;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.unitri.ppi2.rest.domain.Avaria;
-import org.unitri.ppi2.rest.domain.Categoria;
-import org.unitri.ppi2.rest.domain.Cliente;
-import org.unitri.ppi2.rest.domain.Funcionario;
-import org.unitri.ppi2.rest.domain.Locacao;
-import org.unitri.ppi2.rest.domain.Multa;
 import org.unitri.ppi2.rest.domain.Veiculo;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -31,16 +22,13 @@ public class LocacaoTest {
 	private static String URL = "http://localhost:8480/Webproject/rest";
 	
 
-	/*
-	 * Veiculo
-	 */
-	
 	@Test
-	public void test01_createVeiculo(){
+	public void test01_postMethod(){
 
 		ClientConfig clientConfig = new ClientConfig();
 		Client client = ClientBuilder.newClient(clientConfig);
 
+		//client.register(GsonMessageBodyHandler.class);
 		WebTarget target = client.target(URL).path("/veiculo");
 
 		Veiculo v = new Veiculo();
@@ -52,185 +40,13 @@ public class LocacaoTest {
 		Response response = target.request(MediaType.APPLICATION_JSON).post(entity, Response.class);
 		int status = response.getStatus();
 
-		System.out.println("[Veiculo] POST: "+status);
+		System.out.println("POST: "+status);
+		//System.out.println(response);
 		
 		Veiculo veiculo = response.readEntity(Veiculo.class);
+		System.out.println(veiculo);
 		
 		assertEquals(200, status);
 		assertEquals(veiculo.getIdVeiculo(), 1);
-		assertEquals(veiculo.getMarca(), "Ford");
-	}
-	
-	/*
-	 * Categoria
-	 */
-	@Test
-	public void test02_createCategoria(){
-
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-
-		WebTarget target = client.target(URL).path("/categoria");
-
-		Categoria c = new Categoria();
-		c.setIdCategoria(1);
-		c.setPreco(new BigDecimal(1000));
-
-		Entity<Categoria> entity = Entity.entity(c, MediaType.APPLICATION_JSON); 
-		
-		Response response = target.request(MediaType.APPLICATION_JSON).post(entity, Response.class);
-		int status = response.getStatus();
-
-		System.out.println("[Categoria] POST: "+status);
-		
-		Categoria categoria = response.readEntity(Categoria.class);
-		
-		assertEquals(200, status);
-		assertEquals(categoria.getIdCategoria(), 1);
-		assertEquals(categoria.getPreco(), new BigDecimal(1000));
-	}
-	
-	/*
-	 * Avaria
-	 */
-	@Test
-	public void test03_createAvaria(){
-
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-
-		WebTarget target = client.target(URL).path("/avaria");
-
-		Avaria a = new Avaria();
-		a.setIdAvaria(1);
-		a.setDescricao("Farol quebrado");
-
-		Entity<Avaria> entity = Entity.entity(a, MediaType.APPLICATION_JSON); 
-		
-		Response response = target.request(MediaType.APPLICATION_JSON).post(entity, Response.class);
-		int status = response.getStatus();
-
-		System.out.println("[Avaria] POST: "+status);
-		
-		Avaria avaria = response.readEntity(Avaria.class);
-		
-		assertEquals(200, status);
-		assertEquals(avaria.getIdAvaria(), 1);
-		assertEquals(avaria.getDescricao(), "Farol quebrado");
-	}
-	
-
-	/*
-	 * Multa
-	 */
-	@Test
-	public void test04_createMulta(){
-
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-
-		WebTarget target = client.target(URL).path("/multa");
-
-		Multa m = new Multa();
-		m.setIdMulta(1);
-		m.setDescricao("Estacionar em local proibido");
-
-		Entity<Multa> entity = Entity.entity(m, MediaType.APPLICATION_JSON); 
-		
-		Response response = target.request(MediaType.APPLICATION_JSON).post(entity, Response.class);
-		int status = response.getStatus();
-
-		System.out.println("[Multa] POST: "+status);
-		
-		Multa multa = response.readEntity(Multa.class);
-		
-		assertEquals(200, status);
-		assertEquals(multa.getIdMulta(), 1);
-		assertEquals(multa.getDescricao(), "Estacionar em local proibido");
-	}
-	
-	/*
-	 * Cliente
-	 */
-	@Test
-	public void test05_createCliente(){
-
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-
-		WebTarget target = client.target(URL).path("/cliente");
-
-		Cliente c = new Cliente();
-		c.setIdCliente(1);
-		c.setNome("Joaquim da Silva");
-
-		Entity<Cliente> entity = Entity.entity(c, MediaType.APPLICATION_JSON); 
-		
-		Response response = target.request(MediaType.APPLICATION_JSON).post(entity, Response.class);
-		int status = response.getStatus();
-
-		System.out.println("[Cliente] POST: "+status);
-		
-		Cliente cliente = response.readEntity(Cliente.class);
-		
-		assertEquals(200, status);
-		assertEquals(cliente.getIdCliente(), 1);
-		assertEquals(cliente.getNome(), "Joaquim da Silva");
-	}
-	
-	/*
-	 * Funcionario
-	 */
-	@Test
-	public void test06_createFuncionario(){
-
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-
-		WebTarget target = client.target(URL).path("/funcionario");
-
-		Funcionario f = new Funcionario();
-		f.setIdfuncionario(1);
-		f.setMatricula("1234");
-
-		Entity<Funcionario> entity = Entity.entity(f, MediaType.APPLICATION_JSON); 
-		
-		Response response = target.request(MediaType.APPLICATION_JSON).post(entity, Response.class);
-		int status = response.getStatus();
-
-		System.out.println("[Funcionario] POST: "+status);
-		
-		Funcionario funcionario = response.readEntity(Funcionario.class);
-		
-		assertEquals(200, status);
-		assertEquals(funcionario.getIdfuncionario(), 1);
-		assertEquals(funcionario.getMatricula(), "1234");
-	}
-	
-	/*
-	 * Locacao
-	 */
-	@Test
-	public void test07_createLocacao(){
-
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-
-		WebTarget target = client.target(URL).path("/locacao");
-
-		Locacao l = new Locacao();
-		l.setIdLocacao(1);
-
-		Entity<Locacao> entity = Entity.entity(l, MediaType.APPLICATION_JSON); 
-		
-		Response response = target.request(MediaType.APPLICATION_JSON).post(entity, Response.class);
-		int status = response.getStatus();
-
-		System.out.println("[Locacao] POST: "+status);
-		
-		Locacao locacao = response.readEntity(Locacao.class);
-		
-		assertEquals(200, status);
-		assertEquals(locacao.getIdLocacao(), 1);
 	}
 }
