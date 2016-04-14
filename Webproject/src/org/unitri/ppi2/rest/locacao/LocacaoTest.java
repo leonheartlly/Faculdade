51259d6ee8d8497c895277102d23bdbe5a73e75c
tatilattanzi <lattanzi.tati@gmail.com>
2016-04-14 20:29:07 -0300

    Revert "Testes com Junit"
    This reverts commit 841581a6c467256d89ac3f8c07995e60d98911b4.

--- Webproject/src/org/unitri/ppi2/rest/locacao/LocacaoTest.java
+++ Webproject/src/org/unitri/ppi2/rest/locacao/LocacaoTest.java
@@ -1,52 +1,48 @@
 package org.unitri.ppi2.rest.locacao;
 
-import static org.junit.Assert.*;
+import java.net.URI;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-import org.glassfish.jersey.client.ClientConfig;
-import org.junit.FixMethodOrder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
-import org.unitri.ppi2.rest.domain.Veiculo;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class LocacaoTest {	
+public class LocacaoTest {
+	
 	/**
 	 * URL utilizada para testes.
 	 */
-	private static String URL = "http://localhost:8480/Webproject/rest";
+	public static String URL = "http://localhost:8180/Webproject/rest";
 	
+	public static HttpServer server;
 
-	@Test
-	public void test01_postMethod(){
-
-		ClientConfig clientConfig = new ClientConfig();
-		Client client = ClientBuilder.newClient(clientConfig);
-
-		//client.register(GsonMessageBodyHandler.class);
-		WebTarget target = client.target(URL).path("/veiculo");
-
-		Veiculo v = new Veiculo();
-		v.setIdVeiculo(1);
-		v.setMarca("Ford");
-
-		Entity<Veiculo> entity = Entity.entity(v, MediaType.APPLICATION_JSON); 
-		
-		Response response = target.request(MediaType.APPLICATION_JSON).post(entity, Response.class);
-		int status = response.getStatus();
-
-		System.out.println("POST: "+status);
-		//System.out.println(response);
-		
-		Veiculo veiculo = response.readEntity(Veiculo.class);
-		System.out.println(veiculo);
-		
-		assertEquals(200, status);
-		assertEquals(veiculo.getIdVeiculo(), 1);
+	@BeforeClass
+	public static void runBeforeClass(){
+		//Inicia servico
+		ResourceConfig config = new ResourceConfig().packages("org.unitri.ppi2.rest");
+	    URI uri = URI.create(URL);
+	    server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
 	}
+	
+	@AfterClass
+	public static void runAfterClass(){
+		server.shutdown();
+	}
+	
+	@Test
+	public void test() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL);
+		
+		String resultado = target.path("/veiculo/1").request().get(String.class);
+		Assert.assertEquals("idVeiculo:1,marca:ford focus", resultado);
+	}
+	
 }
